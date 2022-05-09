51818f0b74058b42470edd36f23073e580d7c1a9
Nastya <anastasiaalek67@yandex.ru>
2022-05-09 09:00:54 +0300

    3.2. Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,10 +3,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        double procent;
         while (amount > 0) {
-            procent = amount * (percent / 100);
-            amount = amount + procent - salary;
+            amount = amount + amount * (percent / 100) - salary;
             year++;
         }
         return year;
