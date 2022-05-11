1acbb306b68ccbf43e71977c31ecd994aa30200d
Nastya <anastasiaalek67@yandex.ru>
2022-05-11 22:22:54 +0300

    3.2. Загрузка файлов на github [#243562]

---
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+public class AlgoArray {
+    public static int[] exchange(int[] massiv, int first, int second) {
+        int temp = 0;
+        for (int index = 0; index < massiv.length; index++) {
+            temp = massiv[first];
+            massiv[first] = massiv[second];
+            massiv[second] = temp;
+        }
+    return massiv;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[] {5, 3, 2, 1, 4};
+        exchange(array, 0, 3);
+        exchange(array, 1, 2);
+        exchange(array, 3, 4);
+
+        for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
+        }
+    }
+}
