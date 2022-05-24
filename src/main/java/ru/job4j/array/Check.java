fb848b25358e29d2ae152ead0c7a4fad6d603450
Nastya <anastasiaalek67@yandex.ru>
2022-05-24 09:34:22 +0300

    3.2. Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -3,17 +3,10 @@ package ru.job4j.array;
 public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
-        int tru = 0;
-        int fals = 0;
         for (int index = 0; index < data.length; index++) {
-            if (data[index]) {
-                tru += 1;
-            } else {
-                fals += 1;
+            if (data[index] != data[0]) {
+                result = false;
             }
-        }
-        if (fals != data.length &&  tru != data.length) {
-            result = false;
         }
         return result;
     }
