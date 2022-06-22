08ee6e7f511b9b5a7d1fb4c1612672139134777a
Nastya <anastasiaalek67@yandex.ru>
2022-06-22 12:23:07 +0300

    3.2. Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -3,10 +3,10 @@ package ru.job4j.array;
 public class EndsWith {
     @SuppressWarnings("checkstyle:SimplifyBooleanExpression")
     public static boolean endsWith(char[] word, char[] post) {
-        boolean result = false;
+        boolean result = true;
         for (int index = 0; index < post.length; index++) {
-            result = word[word.length - 1 - index] == post[post.length - 1 - index];
-            if (!result) {
+            if (word[word.length - 1 - index] != post[post.length - 1 - index]) {
+                result = false;
                 break;
             }
         }
